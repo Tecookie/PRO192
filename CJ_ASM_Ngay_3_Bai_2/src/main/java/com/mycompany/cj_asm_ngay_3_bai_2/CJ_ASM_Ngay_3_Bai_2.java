7fa70cce903ee352d828faf8ac8e1b3018818627
Tec <quanglikecookie@gmail.com>
2024-05-13 22:38:32 +0700

    Ngay 3

---
+++ CJ_ASM_Ngay_3_Bai_2/src/main/java/com/mycompany/cj_asm_ngay_3_bai_2/CJ_ASM_Ngay_3_Bai_2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cj_asm_ngay_3_bai_2;
+
+/**
+ *
+ * @author GIGABYTE
+ */
+public class CJ_ASM_Ngay_3_Bai_2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
